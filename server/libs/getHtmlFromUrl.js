19a788fe27682cf4fe82c3af481c7791b73beb4d
Kieu Long <kieulong1401@gmail.com>
2024-03-20 11:34:09 +0000

    add random user agent and some config about scraping

--- server/libs/getHtmlFromUrl.js
+++ server/libs/getHtmlFromUrl.js
@@ -1,7 +1,25 @@
 const cheerio = require('cheerio')
 const axios = require('axios')
+const userAgent = require('random-useragent')
+const https = require('https')
 
 module.exports = async (url) => {
-    const page = await axios.get(url)
-    return cheerio.load(page.data)
+    const randomUserAgent = userAgent.getRandom()
+
+    try{
+        const page = await axios.request({
+            method: 'GET',
+            url: url,
+            headers: {
+              'User-Agent': randomUserAgent
+            },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            })
+        })
+        return cheerio.load(page.data)
+    } catch(err) {
+        console.log(err)
+    }
+
 }
